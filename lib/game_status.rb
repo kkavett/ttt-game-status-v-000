cf730d45e5eb962e3cd28d77e98d20cb36b0df3a
Katie Kavett <katie.kavett@flatironschool.com>
2018-09-10 00:18:23 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -27,7 +27,9 @@ def won?(board)
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
       return win_combination
-    else
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combination
+    else  
       false
     end
   end
